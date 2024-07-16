c257203a4dc9609ac47c96dbe14fb9642bc1064f
leobewater <mmbee888@gmail.com>
2024-07-16 15:37:43 -0400

    set up form use client

--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -25,7 +25,7 @@ export default function SnippetCreatePage() {
 
   return (
     <form action={createSnippet}>
-      <h3 className='font-bold m-3'>Create a Snippet</h3>
+      <h3 className='font-bold my-3'>Create a Snippet</h3>
       <div className='flex flex-col gap-4'>
         <div className='flex gap-4'>
           <label htmlFor='title' className='w-12'>
